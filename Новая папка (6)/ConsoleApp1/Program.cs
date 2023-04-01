a3ee26b874c067745338831974d8cf8718ba9983
Lilosh <105058964+LiloshProger@users.noreply.github.com>
2023-04-01 16:26:44 +0300

    Add files via upload

---
+++ Новая папка (6)/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+string rez = @"Result.txt";
+string path = @"Input.txt";
+
+
+
+// асинхронное чтение
+using (StreamReader reader = new StreamReader(path))
+{
+    string text = await reader.ReadToEndAsync();
+    Console.WriteLine(text);
+
+    string[] words = Regex.Split(text, @"\s+");
+    int wor = words.Select(x => x.Length).OrderBy(x => x).First();
+
+    string b= "Слово- " + string.Join("\r\n", words.Where(x => x.Length == wor).GroupBy(x => x).Select(x => string.Concat(x.Key, "; кол-во раз попадается в тексте- ", x.Count())));
+    Console.WriteLine(b);
+
+
+using (StreamWriter writer = new StreamWriter(rez, false))
+    {
+        await writer.WriteLineAsync(text);
+        await writer.WriteLineAsync(b);
+
+    }
+}
